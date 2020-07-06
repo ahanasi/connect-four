2bc4e9f7286bb675096c060bd0c84c1dea5ed0eb
Ahana <ahanasi@seas.upenn.edu>
2020-07-05 20:13:02 -0700

    Initial Commit

---
+++ lib/grid.rb
@@ -0,0 +1,75 @@
+require "pry"
+
+class Grid
+  attr_accessor :grid
+  ROWS = ("1".."6").to_a
+  COLS = ("1".."7").to_a
+  MARKERS = ["🔴", "⬜"]
+
+  def initialize
+    @grid = Array.new(6, ".").map { |row| Array.new(7, ".") }
+  end
+
+  def display_grid()
+    print "\t"
+    print COLS.join("\t")
+    puts
+    @grid.each_with_index do |row, i|
+      puts ("\n")
+      print ROWS[i]
+      print "\t"
+      print row.join("\t")
+      puts
+    end
+  end
+
+  def place_disc(marker, column)
+    @grid.to_enum.with_index.reverse_each do |row, idx|
+      next if is_occupied?([idx, column - 1])
+      return @grid[idx][column - 1] = marker
+    end
+  end
+
+  def is_occupied?(position)
+    MARKERS.any? { |marker| @grid[position[0]][position[1]] == marker }
+  end
+
+  def quartet_check(arr)
+    arr.each do |row|
+      row.each_cons(4).to_a.each do |quartet|
+        if quartet.all? { |val| val == MARKERS[0] } || quartet.all? { |val| val == MARKERS[1] }
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def get_diagonals(matrix)
+    return (0..matrix.size - 4).map { |row| (0..matrix.size - 1 - row).map { |col| matrix[row + col][col] } }.concat(
+      (1..matrix.first.size - 4).map { |col| (0..matrix.first.size - 1 - col).map { |row| matrix[row][col + row] } }
+    )
+  end
+
+  def rotate_array(arr)
+    rotated = []
+    arr.transpose.each {|row| rotated << row.reverse}
+    return rotated
+  end
+
+  def victory_condition
+    # Four in a row
+    return true if quartet_check(@grid)
+
+    # Four in a column
+    return true if quartet_check(@grid.transpose)
+
+    #Four in a diagonal
+    return true if quartet_check(get_diagonals(@grid))
+
+    #Four in anti-diagonal
+    return true if quartet_check(get_diagonals(rotate_array(@grid)))
+
+    return false
+  end
+end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,69 @@
+require "./lib/grid.rb"
+
+describe Grid do
+  describe "#place_disc" do
+    it "drops disc at specified column position" do
+      test = Grid.new()
+      test.place_disc("🔴", 1)
+      expect(test.grid[5][0]).to eql("🔴")
+    end
+
+    it "drops disc above occupied position in column" do
+      test = Grid.new()
+      test.place_disc("🔴", 1)
+      test.place_disc("⬜", 1)
+      expect(test.grid[4][0]).to eql("⬜")
+    end
+  end
+  describe "#is_occupied?" do
+    it "returns true if position is occupied by marker" do
+      test = Grid.new()
+      test.grid[3][3] = "🔴"
+      expect(test.is_occupied?([3, 3])).to be true
+      expect(test.is_occupied?([0, 0])).to be false
+
+      test.grid[0][0] = "⬜"
+      expect(test.is_occupied?([0, 0])).to be true
+    end
+  end
+
+  describe "quartet_check" do
+    it "returns true if 4 consecutive elements are similar" do
+      test = Grid.new()
+      expect(test.quartet_check([["🔴","🔴","🔴","🔴"],[".",".",".","."]])).to be true
+      expect(test.quartet_check([["🔴","🔴",".","🔴",],["🔴","🔴"]])).to be false
+      expect(test.quartet_check([["🔴",".","🔴","🔴","🔴","🔴"],[".","."]])).to be true
+    end
+  end
+
+  describe "#victory_condition" do
+    it "returns true if horizontal 4-in-a-row" do
+      test = Grid.new()
+      test.place_disc("⬜", 3)
+      (3..6).each { |num| test.place_disc("🔴", num) }
+
+      expect(test.victory_condition).to be false
+
+      (4..6).each { |num| test.place_disc("🔴", num) }
+      expect(test.victory_condition).to be true
+    end
+
+    it "returns true if vertical 4-in-a-row" do
+      test = Grid.new()
+      test.place_disc("⬜", 3)
+      3.times { test.place_disc("🔴", 3) }
+
+      expect(test.victory_condition).to be false
+
+      test.place_disc("🔴", 3)
+      expect(test.victory_condition).to be true
+    end
+
+    it "returns true if diagonal 4-in-a-row" do
+      test = Grid.new()
+      (0..3).each {|val| test.grid[val][val] = "🔴"}
+      expect(test.victory_condition).to be true
+    end
+
+  end
+end
