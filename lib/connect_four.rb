755d4148f82cb95b9af5f75f8dcfa5fbd9c9fffc
Ahana <ahanasi@seas.upenn.edu>
2020-07-05 20:44:08 -0700

    Add driver functions

---
+++ lib/connect_four.rb
@@ -0,0 +1,50 @@
+require "./player.rb"
+require "./grid.rb"
+class ConnectFour
+  attr_accessor :grid :player_1 :player_2
+  def initialize(grid, player_1, player_2)
+    @grid = grid
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def play_round(player)
+    puts "#{player.name}, choose a column to drop your marker through."
+    while 1
+      begin
+        player.position = Kernel.gets.chomp.match(/^[1-7]$/)[0].to_i
+      rescue NoMethodError
+        puts "Please choose a column from the grid"
+      else
+        break
+      end
+    end
+    @grid.place_disc(player.marker, player.position)
+    @grid.display_grid
+  end
+
+  def driver
+
+    is_player2 = false
+
+    puts "Welcome to Connect Four!"
+    puts "Player 1, identify yourself!"
+    player_1 = Player.new(gets.chomp, "🔴")
+
+    puts "Player 2, please enter your name: "
+    player2 = Player.new(gets.chomp, "⬜")
+
+    game = ConnectFour.new(Grid.new(), player_1, player_2)
+
+    @grid.display_grid()
+
+    until (@grid.victory_condition())
+      player = is_player2 ? player2 : player1
+      game.play_round(player)
+      is_player2 = !is_player2
+    end
+
+    puts "#{player.name} wins!"
+
+  end
+end
